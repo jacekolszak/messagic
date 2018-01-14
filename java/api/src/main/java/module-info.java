b10a5b3c9cc83839f6ce17e329cffda5c54bf49f
Jacek Olszak <jacek.olszak@4finance.com>
2018-01-14 23:44:09 +0100

    Remove module-info files - Gradle and Idea does not work at the moment

--- java/api/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-// Does not work in Gradle yet
-// module com.github.jacekolszak.messagic {
-//    exports com.github.jacekolszak.messagic;
-//    requires java.base;
-//}
--- java/streams/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-// Does not work in Gradle yet
-// module com.github.jacekolszak.messagic.streams {
-//    exports com.github.jacekolszak.messagic.streams;
-//    requires java.base;
-//    requires com.github.jacekolszak.messagic;
-//}
